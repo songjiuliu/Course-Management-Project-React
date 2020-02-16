d282821a0b12bcf9300e2d37502219e5bbaca98f
songjiuliu <songjiuliu@hotmail.com>
2020-02-16 16:07:54 -0500

    change url

--- src/common/constants.js
+++ src/common/constants.js
@@ -5,10 +5,10 @@ export const COURSES_API_URL = "https://wbdv-generic-server.herokuapp.com/api/so
 export const MODULES_API_URL = "https://wbdv-generic-server.herokuapp.com/api/songjiu/modules"
 export const LESSONS_API_URL = "https://wbdv-generic-server.herokuapp.com/api/songjiu/lessons"
 export const TOPICS_API_URL = "https://wbdv-generic-server.herokuapp.com/api/songjiu/topics"
-export const WIDGETS_API_URL = "http://localhost:8080/api/widgets"
+export const WIDGETS_API_URL = "https://wbdv-generic-server.herokuapp.com/api/widgets"
 
 
 export const COURSES_MODULES_API_URL = (courseId) => `https://wbdv-generic-server.herokuapp.com/api/songjiu/courses/${courseId}/modules`
 export const MODULES_LESSONS_API_URL = (moduleId) => `https://wbdv-generic-server.herokuapp.com/api/songjiu/modules/${moduleId}/lessons`
 export const LESSONS_TOPICS_API_URL = (lessonId) => `https://wbdv-generic-server.herokuapp.com/api/songjiu/lessons/${lessonId}/topics`
-export const TOPICS_WIDGETS_API_URL = (topicId)=> `http://localhost:8080/api/topics/${topicId}/widgets`
+export const TOPICS_WIDGETS_API_URL = (topicId)=> `https://wbdv-generic-server.herokuapp.com/api/topics/${topicId}/widgets`
