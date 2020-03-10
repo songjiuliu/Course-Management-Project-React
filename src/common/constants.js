921dc4167bd2b25f4cb549b6ae99ac0e0ca830c5
songjiuliu <songjiuliu@hotmail.com>
2020-03-10 13:48:20 -0400

    fix bug

--- src/common/constants.js
+++ src/common/constants.js
@@ -4,11 +4,11 @@ export const DEFAULT_CLASS_SIZE = 50
 export const COURSES_API_URL = "https://wbdv-generic-server.herokuapp.com/api/songjiu/courses"
 export const MODULES_API_URL = "https://wbdv-generic-server.herokuapp.com/api/songjiu/modules"
 export const LESSONS_API_URL = "https://wbdv-generic-server.herokuapp.com/api/songjiu/lessons"
-export const TOPICS_API_URL = "http://localhost:8080/api/topics"
-export const WIDGETS_API_URL = "http://localhost:8080/api/widgets"
+export const TOPICS_API_URL = "https://sleepy-badlands-56895.herokuapp.com/api/topics"
+export const WIDGETS_API_URL = "https://sleepy-badlands-56895.herokuapp.com/api/widgets"
 
 
 export const COURSES_MODULES_API_URL = (courseId) => `https://wbdv-generic-server.herokuapp.com/api/songjiu/courses/${courseId}/modules`
 export const MODULES_LESSONS_API_URL = (moduleId) => `https://wbdv-generic-server.herokuapp.com/api/songjiu/modules/${moduleId}/lessons`
-export const LESSONS_TOPICS_API_URL = (lessonId) => `http://localhost:8080/api/lessons/${lessonId}/topics`
-export const TOPICS_WIDGETS_API_URL = (topicId)=> `http://localhost:8080/api/topics/${topicId}/widgets`
+export const LESSONS_TOPICS_API_URL = (lessonId) => `https://sleepy-badlands-56895.herokuapp.com/api/lessons/${lessonId}/topics`
+export const TOPICS_WIDGETS_API_URL = (topicId)=> `https://sleepy-badlands-56895.herokuapp.com/api/topics/${topicId}/widgets`
